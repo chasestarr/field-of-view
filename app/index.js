ccd9c031debd21cbd47b8c46f9124f5eeba36abe
Chase Starr <chasestarr@gmail.com>
2018-02-16 10:09:10 -0800

    simplify build step

--- app/index.js
+++ app/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import { configureStore } from './store/configureStore';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import Routes from './routes';
+import rootReducer from './reducers';
+
 import './app.global.css';
+
+const enhancer = applyMiddleware(thunk);
+function configureStore(initialState?: any) {
+  return createStore(rootReducer, initialState, enhancer);
+}
 
 const store = configureStore();
 

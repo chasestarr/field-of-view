db412ef63ef193ca0dd347b71ad5d9c07b124483
Chase Starr <chasestarr@gmail.com>
2018-02-16 15:40:32 -0800

    reconfigure vault api calls

--- app/utils/vault-api.js
+++ app/utils/vault-api.js
@@ -1,10 +1,28 @@
 import axios from 'axios';
 
-export function login(url, token) {
+import { getToken, getVaultAddress } from './localstorage';
+
+function unauthenticatedRequest(method, path, data = {}) {
   axios.defaults.headers.common['Accept'] = 'application/json';
   axios.defaults.headers.common['Content-Type'] = 'application/json';
   axios.defaults.headers.common['Cache-Control'] = 'no-cache';
+  return axios({ method, url, data });
+}
+
+function authenticatedRequest(token, method, url, data = {}) {
+  axios.defaults.headers.common['Accept'] = 'application/json';
+  axios.defaults.headers.common['Content-Type'] = 'application/json';
+  axios.defaults.headers.common['Cache-Control'] = 'no-cache';
+  axios.defaults.headers.common['X-Vault-Token'] = token;
+  return axios({ method, url, data });
+}
+
+export function login(url, token) {
   const authUrl = `${url}/v1/auth/github/login`;
   const data = { token };
-  return axios({ method: 'POST', url: authUrl, data });
+  return unauthenticatedRequest('POST', authurl, data);
+}
+
+export function isSealed() {
+  return unauthenticatedRequest('GET', '/sys/seal-status').then(res => res.sealed);
 }

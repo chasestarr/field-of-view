171b1c1ebb255cc2865da12183ce02a0e59a33b1
Chase Starr <chasestarr@gmail.com>
2018-02-18 14:47:42 -0800

    fix build

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Text } from '@gigster/pil';
 
 import Content from '../components/Content/index';
-import TitleBar from '../components/TitleBar/index';
+
+import { VaultAddress } from '../utils/localstorage/index';
+
+import styles from './App.css';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <TitleBar />
+        <div className={styles.container}>
+          <Text size="label">{VaultAddress.read()}</Text>
+        </div>
         <Content>{this.props.children}</Content>
       </div>
     );

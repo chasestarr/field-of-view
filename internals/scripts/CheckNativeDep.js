998d7bbba8f4000892e24fc982f6525dfb704de6
Chase Starr <chasestarr@gmail.com>
2018-02-18 14:51:33 -0800

    rename app

--- internals/scripts/CheckNativeDep.js
+++ internals/scripts/CheckNativeDep.js
@@ -8,25 +8,30 @@ import { dependencies } from '../../package.json';
   if (!dependencies) return;
 
   const dependenciesKeys = Object.keys(dependencies);
-  const nativeDeps =
-    fs.readdirSync('node_modules')
-      .filter(folder => fs.existsSync(`node_modules/${folder}/binding.gyp`));
+  const nativeDeps = fs
+    .readdirSync('node_modules')
+    .filter(folder => fs.existsSync(`node_modules/${folder}/binding.gyp`));
 
   try {
     // Find the reason for why the dependency is installed. If it is installed
     // because of a devDependency then that is okay. Warn when it is installed
     // because of a dependency
-    const dependenciesObject = JSON.parse(execSync(`npm ls ${nativeDeps.join(' ')} --json`).toString());
+    const dependenciesObject = JSON.parse(
+      execSync(`npm ls ${nativeDeps.join(' ')} --json`).toString()
+    );
     const rootDependencies = Object.keys(dependenciesObject.dependencies);
-    const filteredRootDependencies = rootDependencies
-      .filter(rootDependency => dependenciesKeys.includes(rootDependency));
+    const filteredRootDependencies = rootDependencies.filter(rootDependency =>
+      dependenciesKeys.includes(rootDependency)
+    );
 
     if (filteredRootDependencies.length > 0) {
       const plural = filteredRootDependencies.length > 1;
       console.log(`
 
 ${chalk.whiteBright.bgYellow.bold('Webpack does not work with native dependencies.')}
-${chalk.bold(filteredRootDependencies.join(', '))} ${plural ? 'are native dependencies' : 'is a native dependency'} and should be installed inside of the "./app" folder.
+${chalk.bold(filteredRootDependencies.join(', '))} ${
+        plural ? 'are native dependencies' : 'is a native dependency'
+      } and should be installed inside of the "./app" folder.
 
 
 First uninstall the packages from "./package.json":
@@ -40,7 +45,9 @@ ${chalk.whiteBright.bgGreen.bold('cd ./app && npm install your-package --save')}
 
 
 Read more about native dependencies at:
-${chalk.bold('https://github.com/chentsulin/electron-react-boilerplate/wiki/Module-Structure----Two-package.json-Structure')}
+${chalk.bold(
+        'https://github.com/chentsulin/field-of-view/wiki/Module-Structure----Two-package.json-Structure'
+      )}
 
 
 `);
